5926be2b1c8626e05839ee5f33303e3f7b7d8200
JuanC <DevJuanC@protonmail.com>
2025-11-16 15:41:58 -0300

    mod: db conection

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,24 @@
-import { Pool } from 'pg';
+import { Pool, PoolConfig } from 'pg';
+
+const myPoolConfig = (): PoolConfig => {
+  if (process.env.SCOPE === 'prod') {
+    return {
+      ssl: true,
+      database: process.env.POSTGRES_DATABASE,
+      host: process.env.POSTGRES_HOST,
+      port: 6543,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+    };
+  }
+
+  return {
+    connectionString: process.env.DATABASE_URL || '',
+  }
+}
 
 // Este objeto Pool será la conexión a Vercel Postgres o tu BD de desarrollo.
 // Lo mantendremos simple para el desarrollo local.
-const db = new Pool({
-  connectionString: process.env.DATABASE_URL || process.env.POSTGRES_URL,
-});
+const db = new Pool(myPoolConfig());
 
 export { db };
