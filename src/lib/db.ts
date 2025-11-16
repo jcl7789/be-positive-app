ef1f1b4d0ebad03c83573537a1a7ea22061b46ca
JuanC <DevJuanC@protonmail.com>
2025-11-16 13:18:42 -0300

    mod: database url

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,7 +3,7 @@ import { Pool } from 'pg';
 // Este objeto Pool será la conexión a Vercel Postgres o tu BD de desarrollo.
 // Lo mantendremos simple para el desarrollo local.
 const db = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || process.env.POSTGRES_URL,
 });
 
 export { db };
